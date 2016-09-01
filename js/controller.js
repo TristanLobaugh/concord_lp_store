55d9984bccdbb82be430c89e1300730b8ba8b545
TristanLobaugh <ttrader_atl@yahoo.com>
2016-09-01 14:50:44 -0400

    update

--- js/controller.js
+++ js/controller.js
@@ -5,10 +5,13 @@ concordApp.controller("concordController", function($scope, $http){
 
 	var search = "";
 
-    var url = "http://eve-marketdata.com/api/item_orders2.json?char_name=demo&buysell=b&type_ids=34,36" + search;
+    // var url = "http://api.eve-central.com/api/quicklook?typeid=3196&typeid=34" + search;
+    var url = "http://api.eve-central.com/api/marketstat?typeid=3196&typeid=35"
     $http.get(url).success(function(data) {
     	console.log(data);
-        $scope.data = data;
+        $scope.data = $.xml2json(data);
+        // var json = $.xml2json(response);
+        console.log($scope.data);
         for (var i = 0; i < $scope.data.length; i++) {
 
         }
